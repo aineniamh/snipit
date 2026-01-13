199e602e516abd5d3422e0b2c90b18bbb5bdfec5
aineniamh <aine.otoole@ed.ac.uk>
2026-01-13 12:16:04 +0000

    repackage project with pyproject.toml now

--- setup.py
+++ setup.py
@@ -1,27 +1,6 @@
-from setuptools import setup, find_packages
-from setuptools.command.build_py import build_py
-import glob
-import os
-import pkg_resources
+from setuptools import setup
 
-from snipit import __version__, _program
+# This file exists only for backward compatibility.
+# Modern installations should use pyproject.toml
 
-setup(name='snipit',
-      version=__version__,
-      packages=find_packages(),
-      scripts=["snipit/scripts/snp_functions.py"],
-      install_requires=[
-            "biopython>=1.70",
-            "matplotlib>=3.2.1"
-        ],
-      description='snipit',
-      url='https://github.com/aineniamh/snipit',
-      author='Aine OToole',
-      author_email='aine.otoole@ed.ac.uk',
-      entry_points="""
-      [console_scripts]
-      {program} = snipit.command:main
-      """.format(program = _program),
-      include_package_data=True,
-      keywords=[],
-      zip_safe=False)
+setup()
